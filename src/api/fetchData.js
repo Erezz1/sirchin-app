581d36482d5e230ce4f6839a0fbbcd45b59d5c2a
Neto Martinez <netostrikr@gmail.com>
2022-03-24 12:38:25 -0600

    API key corregida

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,3 +1,5 @@
+const API_KEY = 'd797cafeafmshf60e275e1a7122bp1c9c9ajsne129711de763'
+
 export const fetchData = async ( query, search = 'search' ) => {
     if ( search === 'videos' ) {
         const response = await fetch(`https://google-search3.p.rapidapi.com/api/v1/search/q=${ query } videos`, {
@@ -6,7 +8,7 @@ export const fetchData = async ( query, search = 'search' ) => {
                 'x-user-agent': 'desktop',
                 'x-proxy-location': 'MX',
                 'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-                'x-rapidapi-key': 'KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA'
+                'x-rapidapi-key': API_KEY
             }
         });
 
@@ -20,7 +22,7 @@ export const fetchData = async ( query, search = 'search' ) => {
                 'x-user-agent': 'desktop',
                 'x-proxy-location': 'EU',
                 'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-                'x-rapidapi-key': 'KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA'
+                'x-rapidapi-key': API_KEY
             }
         });
 
